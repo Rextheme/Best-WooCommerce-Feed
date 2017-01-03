f3108284f99e276e48e40763d3318c7848cd56cc
Khorshed Alam <theKhorshed.alam@gmail.com>
2017-01-03 19:29:50 +0600

    add class for defining metaboxes

---
+++ admin/class-rex-product-feed-metabox.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * @link       https://rextheme.com
+ * @since      1.0.0
+ *
+ * @package    Rex_Product_Metabox
+ * @subpackage Rex_Product_Feed/admin
+ */
+
+/**
+ * The admin-specific functionality of the plugin.
+ *
+ * Defines all the Metaboxes for Products
+ *
+ * @package    Rex_Product_Metabox
+ * @subpackage Rex_Product_Feed/admin
+ * @author     RexTheme <info@rextheme.com>
+ */
+class Rex_Product_Metabox {
+
+}
