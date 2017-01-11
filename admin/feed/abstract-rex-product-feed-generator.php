457c6f0c801b526093f5d0163c5467d2fab6208b
Khorshed Alam <theKhorshed.alam@gmail.com>
2017-01-11 17:32:02 +0600

    Add factory, template method for generating feed and ajax handler.

---
+++ admin/feed/abstract-rex-product-feed-generator.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Abstract Rex Product Feed Generator
+ *
+ * A abstract class definition that includes functions used for generating xml feed.
+ *
+ * @link       https://rextheme.com
+ * @since      1.0.0
+ * The XML Feed Generator.
+ *
+ * This is used to generate xml feed based on given settings.
+ *
+ * @since      1.0.0
+ * @package    Rex_Product_Feed_Abstract_Generator
+ * @author     RexTheme <info@rextheme.com>
+ */
+abstract class Rex_Product_Feed_Abstract_Generator {
+
+
+}
---
+++ admin/feed/class-rex-product-feed-google.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * The file that generates xml feed for Google.
+ *
+ * A class definition that includes functions used for generating xml feed.
+ *
+ * @link       https://rextheme.com
+ * @since      1.0.0
+ *
+ * @package    Rex_Product_Feed_Google
+ * @subpackage Rex_Product_Feed_Google/includes
+ * @author     RexTheme <info@rextheme.com>
+ */
+
+class Rex_Product_Feed_Google extends Rex_Product_Feed_Abstract_Generator {
+
+
+}
