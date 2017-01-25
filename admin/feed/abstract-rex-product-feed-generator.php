2cc0f3384fa036b655618e8ebc2ebd197abcdc62
Khorshed Alam <theKhorshed.alam@gmail.com>
2017-01-25 13:23:38 +0600

    add more functionality to abstarct genarator.

--- admin/feed/abstract-rex-product-feed-generator.php
+++ admin/feed/abstract-rex-product-feed-generator.php
@@ -17,48 +17,127 @@
  */
 abstract class Rex_Product_Feed_Abstract_Generator {
 
-  /**
-   * Get the products to generate feed.
-   *
-   **/
-  protected function get_products( $args = array() ) {
+	/**
+	 * The Product/Feed ID.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      Rex_Product_Feed_Abstract_Generator    id    Feed id.
+	 */
+	protected $id;
 
-    $args = array(
-      'post_type'      => 'product',
-      'posts_per_page' => -1
-    );
+	/**
+	 * The Product Query args to retrieve specific products for making the Feed.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      Rex_Product_Feed_Abstract_Generator    $products_args    Contains products query args for feed.
+	 */
+	protected $products_args;
 
-    $products = get_posts( $args );
+	/**
+	 * The Product Query args to retrieve specific products for making the Feed.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      Rex_Product_Feed_Abstract_Generator    $products    Contains all products to make feed.
+	 */
+	protected $products;
 
-    return $products;
+	/**
+	 * Define the core functionality of the plugin.
+	 *
+	 * Set the plugin name and the plugin version that can be used throughout the plugin.
+	 * Load the dependencies, define the locale, and set the hooks for the admin area and
+	 * the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function __construct( $config ) {
 
-  }
+		$this->id = $config['post_id'];
+		$this->prepare_products_args( $config['products'] );
+		$this->get_products();
+
+	}
+
+	/**
+	 * Prepare the Products Query args for retrieving  products.
+	 *
+	 **/
+	protected function prepare_products_args( $args ) {
+
+		$this->products_args = array(
+			'post_type'      => 'product',
+			'posts_per_page' => -1,
+		);
+
+		if ( $args['products_scope'] !== 'all') {
+			$terms = $args['products_scope'] === 'product_tag' ? 'tags' : 'cats';
+
+			$this->products_args['tax_query'][] = array(
+				'taxonomy' => $args['products_scope'],
+				'field'    => 'slug',
+				'terms'    => $args[$terms]
+			);
+		}
+	}
 
 
-  /**
-   * Get Product data.
-   *
-   * @return array
-   **/
-  protected function get_product_data( $id = false ){
-    $product = new WC_Product($id);
+	/**
+	 * Get the products to generate feed.
+	 *
+	 **/
+	protected function get_products() {
 
-    return array(
-      'id'       => $product->get_id(),
-      'title'    => $product->get_title(),
-      'desc'     => $product->get_post_data()->post_excerpt,
-      'link'     => $product->get_permalink(),
-      'image'    => wp_get_attachment_url( $product->get_image_id() ),
-      'stock'    => 'in stock',// $product->get_availability()['class'],
-      'price'    => $product->get_price(),
-      'currency' => get_woocommerce_currency(),
-    );
-  }
+		$this->products = get_posts( $this->products_args );
 
-  /**
-   * Responsible for creating the feed.
-   * @return string
-   **/
-  abstract protected function make_feed();
+	}
+
+
+	/**
+	 * Get Product data.
+	 *
+	 * @return array
+	 **/
+	protected function get_product_data( $id = false ){
+		$product = new WC_Product($id);
+
+		return array(
+			'id'       => $product->get_id(),
+			'title'    => $product->get_title(),
+			'desc'     => $product->get_post_data()->post_excerpt,
+			'link'     => $product->get_permalink(),
+			'image'    => wp_get_attachment_url( $product->get_image_id() ),
+			'stock'    => 'in stock',// $product->get_availability()['class'],
+			'price'    => $product->get_price(),
+			'currency' => get_woocommerce_currency(),
+		);
+	}
+
+	/**
+	 * Save the feed as XML file.
+	 *
+	 * @return boolean
+	 **/
+	protected function save_feed( $feed ){
+		$path  = wp_upload_dir();
+		$path  = $path['basedir'] . '/rex-feed';
+
+		// make directory if not exist
+		if ( !file_exists($path) ) {
+			wp_mkdir_p($path);
+		}
+
+		$file = trailingslashit($path) . "feed-{$this->id}.xml";
+
+		return (bool) file_put_contents($file, $feed);
+	}
+
+	/**
+	 * Responsible for creating the feed.
+	 * @return string
+	 **/
+	abstract public function make_feed();
 
 }
