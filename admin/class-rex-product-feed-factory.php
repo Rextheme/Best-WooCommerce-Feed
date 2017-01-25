f783c06beb50b0b2dab18b91fb799ced758b8b91
Khorshed Alam <theKhorshed.alam@gmail.com>
2017-01-25 13:22:29 +0600

    pass config to constructor

--- admin/class-rex-product-feed-factory.php
+++ admin/class-rex-product-feed-factory.php
@@ -11,17 +11,17 @@
  */
 class Rex_Product_Feed_Factory {
 
-  public static function build( $merchant ){
-    $className = 'Rex_Product_Feed_'. ucfirst( $merchant );
+	public static function build( $config ){
+		$className = 'Rex_Product_Feed_'. ucfirst( $config['merchant'] );
 
-    if( $merchant == '' || ! class_exists( $className ) ) {
-      throw new Exception('Invalid Merchant.');
-    } else {
-      return new $className();
-    }
+		if( $config == '' || ! class_exists( $className ) ) {
+			throw new Exception('Invalid Merchant.');
+		} else {
+			return new $className( $config );
+		}
 
-    return false;
-  }
+		return false;
+	}
 
 
 
