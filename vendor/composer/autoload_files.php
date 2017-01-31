1942e842e88475e314cbab8962595268d45f8081
Khorshed Alam <theKhorshed.alam@gmail.com>
2017-01-31 11:49:37 +0600

    remove useless dependencies.

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -6,10 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'a0edc8309cc5e1d60e3047b5df6b7052' => $vendorDir . '/guzzlehttp/psr7/src/functions_include.php',
-    'c964ee0ededf28c96ebd9db5099ef910' => $vendorDir . '/guzzlehttp/promises/src/functions_include.php',
-    '37a3dc5111fe8f707ab4c132ef1dbc62' => $vendorDir . '/guzzlehttp/guzzle/src/functions_include.php',
-    'decc78cc4436b1292c6c0d151b19445c' => $vendorDir . '/phpseclib/phpseclib/phpseclib/bootstrap.php',
     'db1766888a4f96ab813d6f6a38125eb9' => $vendorDir . '/philipnewcomer/wp-ajax-helper/src/functions.php',
     '61b9c0b2c629ace5be0b7ec3e2b7b6b4' => $vendorDir . '/johnbillion/extended-cpts/extended-cpts.php',
     '10e2bd795825f9abb125ae7cc3251364' => $vendorDir . '/webdevstudios/cmb2/init.php',
