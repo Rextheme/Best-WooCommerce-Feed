16a74b406aff7b1cd47f199ec1dfa9b6c7c0b404
Khorshed Alam <theKhorshed.alam@gmail.com>
2017-03-09 18:13:51 +0600

    Template for displaying feed config template.

---
+++ admin/partials/feed-config-metabox-display.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is display the custom feed configuration part of the metabox on feed edit screen.
+ *
+ * @link       https://rextheme.com
+ * @since      1.0.0
+ *
+ * @package    Rex_Product_Feed
+ * @subpackage Rex_Product_Feed/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
+
+<div id="rex-feed-config">
+  <table id="config-table">
+    <thead>
+      <tr>
+        <th>Attributes</th>
+        <th>Type</th>
+        <th>Value</th>
+        <th>Prefix</th>
+        <th>Suffix</th>
+        <th>Output Sanitization</th>
+        <th>Output Limit</th>
+        <th></th>
+      </tr>
+    </thead>
+
+    <tbody>
+
+    <?php foreach ( $gt->getTemplateMappings() as $key => $item): ?>
+      <tr data-row-id="<?php echo $key; ?>">
+        <td><?php $gt->printSelectDropdown( $key, 'attributes', $item['attr'] ); ?></td>
+        <td><?php $gt->printAttType( $key, $item['type'] ); ?></td>
+        <td><?php $gt->printSelectDropdown( 'meta_keys', $item['attr'] ); ?></td>
+        <td><?php $gt->printInput( $key, 'prefix', $item['prefix'] ); ?></td>
+        <td><?php $gt->printInput( $key, 'suffix', $item['suffix'] ); ?></td>
+        <td><?php $gt->printSelectDropdown( $key, 'sanitization_options', $item['escape'] ); ?></td>
+        <td><?php $gt->printInput( $key, 'limit', $item['limit'] ); ?></td>
+        <td><a class="button delete">Delete</a></td>
+      </tr>
+    <?php endforeach ?>
+
+    </tbody>
+
+  </table>
+  <a id="rex-new-attr" class="button">Add New Attribute</a>
+
+</div>
